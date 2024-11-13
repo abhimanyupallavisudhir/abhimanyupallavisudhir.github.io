bc7f4d914c2914ed5f80c89a39c0674f0c81aa5b
abhimanyu <abhimanyupallavisudhir@gmail.com>
2024-11-13 01:14:49 +0000

    add docx conversion

---
+++ pre-commit.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Copy relevant/cv.pdf to the root directory as relevant.pdf
+cp -f cv/relevant/cv.pdf cv/relevant.pdf
+cp -f cv/extended/cv.pdf cv/extended.pdf
+
+pandoc -s --toc cv/relevant/cv.tex -o cv/relevant.html
+# pandoc -s --toc --template="C:\Users\abhim\Google Drive\Gittable\Code\abhimanyupallavisudhir.github.io\cv\cv_template.html" cv/relevant/cv.tex -o cv/relevant.html 
+pandoc -s cv/relevant/cv.tex -o cv/relevant.docx
+
+# Add relevant.pdf to the staging area so that it gets committed
+git add cv/relevant.pdf
+git add cv/extended.pdf
+git add cv/relevant.html
+git add cv/relevant.docx
+
+# Check if the copy succeeded
+if [ $? -ne 0 ]; then
+ echo "Failed a precommit operation (see git/hooks/pre-commit)"
+ exit 1
+else
+ echo "Succeeded precommit operations"
+fi
